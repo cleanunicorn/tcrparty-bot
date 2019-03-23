cd81477183f3afb615ff4c010b9548ee1714660b
Daniel Luca <lucadanielcostin@gmail.com>
2019-03-23 11:15:37 +0200

    Sleep and format source code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import twitter
 import json
+import time
 
 with open("tokens.json") as f:
     tokens = f.read()
@@ -21,10 +22,12 @@ print(api.VerifyCredentials())
 # End
 
 while True:
+    fib1, fib2 = 1, 1
+
     # Get the latest message id
     dms = api.GetDirectMessages(skip_status=True, full_text=True, return_json=True)
     last_dm = dms[len(dms) - 1]
-    last_dm_id = last_dm['message_id'] # TODO: Replace with correct key
+    last_dm_id = last_dm["message_id"]  # TODO: Replace with correct key
 
     # Request tokens from faucet
     dm = api.PostDirectMessage("faucet", user_id="tcrparty", return_json=True)
@@ -35,15 +38,29 @@ while True:
     confirmed = False
     retry = False
     while replied == False:
-        dms = api.GetDirectMessages(since_id=last_dm_id, skip_status=True, full_text=True, return_json=True)
+        fib1, fib2 = fibonacci(fib1, fib2)
+        time.sleep(fib2 * 60)  # Sleep fibonacci minutes
+
+        dms = api.GetDirectMessages(
+            since_id=last_dm_id, skip_status=True, full_text=True, return_json=True
+        )
         dms = json.load(dms)
         for dm in dms:
-            if dm['user_id'] == 'tcrparty': # TODO: Replace with user_id
+            if dm["user_id"] == "tcrparty":  # TODO: Replace with user_id
                 replied = True
-                if dm['message'].find("Your message is confirmed"): # TODO: Replace with key and text
+                if dm["message"].find(
+                    "Your message is confirmed"
+                ):  # TODO: Replace with key and text
                     confirmed = True
-                if dm['message'].find("Too early to send message"):  # TODO: Replace with key and text
+                if dm["message"].find(
+                    "Too early to send message"
+                ):  # TODO: Replace with key and text
                     retry = True
 
     if retry == False:
-        break
+        time.sleep(86400)  # Sleep 1 day
+
+
+def fibonacci(a, b):
+    return (b, a + b)
+
