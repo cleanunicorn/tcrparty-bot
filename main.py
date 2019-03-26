6fc18177525785d319951694b123681327d1672a
Daniel Luca <lucadanielcostin@gmail.com>
2019-03-26 07:37:39 +0000

    Does not run forever. It ends after a successful faucet + 24h sleep

--- main.py
+++ main.py
@@ -24,48 +24,47 @@ api = twitter.Api(
 logging.info(api.VerifyCredentials() is not None)
 
 retry_sleep = ("10", "seconds")
-while True:
-    # Get the latest message id
-    logging.info("Getting direct messages")
-    dms = api.GetDirectMessages(skip_status=True, full_text=True, return_json=True)
-    last_dm = dms["events"][0]
-    last_dm_id = last_dm["id"]
+# Get the latest message id
+logging.info("Getting direct messages")
+dms = api.GetDirectMessages(skip_status=True, full_text=True, return_json=True)
+last_dm = dms["events"][0]
+last_dm_id = last_dm["id"]
 
-    # Request tokens from faucet
-    logging.info("Sending 'faucet' to tcrpartyvip")
-    dm = api.PostDirectMessage("faucet", screen_name="tcrpartyvip", return_json=True)
-    time.sleep(60) # Wait for a reply
+# Request tokens from faucet
+logging.info("Sending 'faucet' to tcrpartyvip")
+dm = api.PostDirectMessage("faucet", screen_name="tcrpartyvip", return_json=True)
+time.sleep(60) # Wait for a reply
 
-    # Wait for message confirmation
-    replied = False
-    dms = api.GetDirectMessages(
-        since_id=last_dm_id, skip_status=True, full_text=True, return_json=True
-    )
-    for dm in dms["events"]:
-        if dm["message_create"]["sender_id"] == "1029028522843627520":
-            message_text = dm["message_create"]["message_data"]["text"]
-            logging.info("Found reply: {}".format(message_text))
-            if message_text.find("You got it.") != -1:
-                replied = True
-                retry_sleep = ('24', 'hours')
-                logging.info("Received tokens")
+# Wait for message confirmation
+replied = False
+dms = api.GetDirectMessages(
+    since_id=last_dm_id, skip_status=True, full_text=True, return_json=True
+)
+for dm in dms["events"]:
+    if dm["message_create"]["sender_id"] == "1029028522843627520":
+        message_text = dm["message_create"]["message_data"]["text"]
+        logging.info("Found reply: {}".format(message_text))
+        if message_text.find("You got it.") != -1:
+            replied = True
+            retry_sleep = ('24', 'hours')
+            logging.info("Received tokens")
 
-            regex_match_time = (
-                r"Try again ([0-9]+) (hours|minutes|seconds) from now."
-            )
-            matches = re.search(regex_match_time, message_text)
-            if matches is not None:
-                replied = True
-                retry_sleep = matches.groups()
-                logging.info("Too early to get tokens")
-            
-        if replied is True:
-            break
+        regex_match_time = (
+            r"Try again ([0-9]+) (hours|minutes|seconds) from now."
+        )
+        matches = re.search(regex_match_time, message_text)
+        if matches is not None:
+            replied = True
+            retry_sleep = matches.groups()
+            logging.info("Too early to get tokens")
+        
+    if replied is True:
+        break
 
-    logging.info("Sleeping {} {}".format(retry_sleep[0], retry_sleep[1]))
-    if retry_sleep[1] == "minutes":
-        time.sleep(int(retry_sleep[0]) * 60 + 60)
-    elif retry_sleep[1] == "hours":
-        time.sleep(int(retry_sleep[0]) * 3600)
-    elif retry_sleep[1] == "seconds":
-        time.sleep(int(retry_sleep[0]) + 60)
+logging.info("Sleeping {} {}".format(retry_sleep[0], retry_sleep[1]))
+if retry_sleep[1] == "minutes":
+    time.sleep(int(retry_sleep[0]) * 60 + 60)
+elif retry_sleep[1] == "hours":
+    time.sleep(int(retry_sleep[0]) * 3600)
+elif retry_sleep[1] == "seconds":
+    time.sleep(int(retry_sleep[0]) + 60)
